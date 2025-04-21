f9c49f5268fb3def157a1c539fa1cdfd298b577b
Rishi Bathina <rishibathina@google.com>
2025-04-21 21:29:50 +0000

    added remove taint logic, now need to check if it works

--- internal/controller/node_controller.go
+++ internal/controller/node_controller.go
@@ -17,22 +17,48 @@ limitations under the License.
 package controller
 
 import (
-	"context"
-	"fmt"
 	"log"
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
 
-	compute "cloud.google.com/go/compute/apiv1"
+	"github.com/prometheus/client_golang/prometheus"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/selection"
+	"nodemanagement.gke.io/maintenance-controller/engine/opportunisticmaintenance"
+	"nodemanagement.gke.io/maintenance-controller/services"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 )
 
 const (
+	nodeName = "Node_Name"
+	//Annotations
+	customerTriggeredMaintenanceAnnotation = "node.gke.io/maintenance-state"
+	reportAndReplaceOperationAnnotation    = "node.gke.io/report-and-replace-operation"
+	//Labels
+	customerTriggeredMaintenanceLabel = "cloud.google.com/perform-maintenance"
+	upcomingMaintenanceLabel          = "cloud.google.com/scheduled-maintenance-time"
+	latestStartTimeLabel              = "cloud.google.com/scheduled-maintenance-time-latest"
+	activeMaintenanceLabel            = "cloud.google.com/active-node-maintenance"
+	machineFamilyLabel                = "cloud.google.com/machine-family"
+	nodePoolLabel                     = "cloud.google.com/gke-nodepool"
+	faultBehaviorLabel                = "cloud.google.com/fault-behavior"
+	reportAndReplaceStatusLabel       = "cloud.google.com/report-and-replace-status"
+	//Taints
+	maintenanceWindowStartTaintKey = "cloud.google.com/maintenance-window-started"
+	nodeTerminationTaintKey        = "cloud.google.com/impending-node-termination"
 	outOfServiceKey = "node.kubernetes.io/out-of-service:NoExecute"
 	outOfServiceEffect = v1.TaintEffectNoExecute
 )
+
+var log = ctrl.Log.WithName("groupMaintenanceController")
 
 // NodeReconciler reconciles a Node object
 type NodeReconciler struct {
@@ -57,7 +83,7 @@ type NodeReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
 func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	// _ = log.FromContext(ctx)
 	log.Info("Reconciling on node")
 
 	n := &v1.node{}
@@ -111,7 +137,7 @@ func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 					break
 				}
 				
-				vmRepairing, err := checkVMRepairing(projectID, zone, instanceName, ctx)
+				vmRepairing, err := r.checkVMRepairing(projectID, zone, instanceName, ctx)
 				if err != nil {
 					log.Error(err, "failed to check if VM is in REPAIRING: ", nodeName)
 					break
@@ -122,9 +148,9 @@ func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 
 					// TODO: Add tainting nodepool 
 					// taint the nodes in the nodepool associated with this node
-					err := patchGroupTaint(n)
+					err := r.patchGroupTaint(ctx, n)
 					if err != nil {
-						log.Error(err, "group taint was not applied")
+						log.Error(err, "group taint was not applied to all nodes")
 						requeueAtEnd = true
 						break
 					}
@@ -175,7 +201,7 @@ func getVMInfo(node *v1.Node) string, string, string {
 }
 
 // Check if the VM instance is in repairing status
-func checkVMRepairing(projectID string, zone string, instanceName string, ctx context.Context) bool, error {
+func (r *NodeReconciler) checkVMRepairing(projectID string, zone string, instanceName string, ctx context.Context) bool, error {
 	instancesClient, err := compute.NewInstancesRESTClient(ctx)
 	if err != nil {
 		log.Error("compute.NewInstancesRESTClient: %v", err)
@@ -211,7 +237,8 @@ func checkVMRepairing(projectID string, zone string, instanceName string, ctx co
 }
 
 // Patches the node with the group taint 
-func patchGroupTaint(n *v1.Node) error {
+func (r *NodeReconciler) patchGroupTaint(ctx context.Context, n *v1.Node) error {
+	// create groupTaint 
 	currentTime := time.Now() 
 	startTime := currentTime.Format(time.RFC3339)
 
@@ -221,7 +248,39 @@ func patchGroupTaint(n *v1.Node) error {
 		Effect: outOfServiceEffect,
 	}
 
-	updatedTaints := append(n.Spec.Taints, *groupTaint)
+	nl, err := listNodesInNodepool(&n)
+	if err != nil {
+		log.Error("Could not get nodes in nodepool")
+		return err
+	}
+
+	// iterate through all the nodes in the pool and taint all except on the current node
+	for _, otherNode := range nl.Items {
+		otherNodePoolName, otherOk := otherNode.Labels[nodePoolLabel]
+		if otherOk && otherNodePoolName == npName && otherNode.Name != n.Name { // checking that the nodepool is the same and it isn't the current node
+			log.Info("Patching node with group taint", "nodeName:", otherNode.Name, "taint:", groupTaint)
+
+			// Patch the node with the group taint
+			if err := r.patchTaint(ctx, &otherNode, groupTaint); err != nil {
+				log.Error(err, "failed to patch node with group taint", "nodeName", otherNode.Name, "taint", groupTaint)
+				return err
+			}
+		}
+	}
+
+	// taint the current node 
+	err := r.patchTaint(ctx, &n, groupTaint)
+	if err != nil {
+		log.Error(err, "failed to patch the current node with group taint", "nodeName", n.Name, "taint", groupTaint)
+        return err
+	}
+
+	return nil
+}
+
+// Patch the node with the requested taint
+func (r *NodeReconciler) patchTaint(ctx context.Context, n *v1.Node, taintToPatch *v1.Taint) error {
+	updatedTaints := append(n.Spec.Taints, *taintToPatch)
 	patch := map[string][]v1.Taint{
 		"spec": {
 			updatedTaints,
@@ -230,17 +289,64 @@ func patchGroupTaint(n *v1.Node) error {
 
 	patchBytes, err := json.Marshal(patch)
 	if err != nil {
-		log.Error(err, "failed to marshal patch for taints update", "nodeName", nodeName, "taint", nodeTermTaint)
+		log.Error(err, "failed to marshal patch for taints update", "nodeName", n.Name, "taint", taintToPatch)
 		return err
 	}
 
 	err = r.Client.Patch(ctx, n, client.RawPatch(types.MergePatchType, patchBytes))
 	if err != nil {
-		log.Error(err, "error applying taint on node using append and patch", "nodeName", nodeName, "taint", nodeTermTaint)
+		log.Error(err, "error applying taint on node using append and patch", "nodeName", n.Name, "taint", taintToPatch)
 		return err
 	}
 
 	return nil
+}
+
+// remove the taints off of the nodepool
+// return true if there is an error, meaning take off the taint anyway
+func removeGroupTaintNp(n *v1.Node, taintKey string) error {
+	nl, err := listNodesInNodepool(&n)
+	if err != nil {
+		log.Error("Unable to list nodes in nodepool for removing taint")
+		return err
+	}
+
+
+	for _, otherNode := range nl.Items {
+		otherNodePoolName, otherOk := otherNode.Labels[nodePoolLabel]
+		if otherOk && otherNodePoolName == npName && otherNode.Name != n.Name { // checking that the nodepool is the same and it isn't the current node
+			log.Info("Removing group taint off of node,", "nodeName:", otherNode.Name, "taint:", groupTaint)
+			otherNode.Spec.Taints = removeTaintKey(taintKey, otherNode.Spec.Taints)
+		}
+	}
+
+	n.Spec.Taints = removeTaintKey(taintKey, n.Spec.Taints)
+	return nil
+}
+
+// remove a specific taint from taints
+// if taint does not exist, return the original list
+func removeTaintKey(searchTaintKey string, taints []v1.Taint) []v1.Taint {
+	index := getTaintIndex(searchTaintKey, taints)
+
+	if index > -1 {
+		return append(taints[:index], taints[index+1:]...)
+	}
+	return taints
+}
+
+// get the index of the taint if it exists
+// otherwise return -1
+func getTaintIndex(searchTaintKey string, taints []v1.Taint) int {
+	index := -1
+	for ind, taint := range taints {
+		if taint.Key == searchTaintKey {
+			index = ind
+			break
+		}
+	}
+
+	return index
 }
 
 // Check if Taint with the given key exists on the node
@@ -265,12 +371,7 @@ func getTaintApplyTime(n *v1.Node, taintKey string) string {
 	return ""
 }
 
-// remove the taints off of the nodepool
-// return true if there is an error, meaning take off the taint anyway
-func removeGroupTaintNp(n *v1.Node, taintKey string) string, error {
-
-}
-
+// Check the taint to see if it has been longer than 2 minutes since application
 func isTaintAppliedTooLong(taintApplyTimeStr string) (bool, error) {
 	if taintApplyTimeStr == "" {
 		return true, fmt.Errorf("taint apply time string is empty")
@@ -285,4 +386,33 @@ func isTaintAppliedTooLong(taintApplyTimeStr string) (bool, error) {
 	durationSinceApply := currentTime.Sub(parsedApplyTime)
 
 	return durationSinceApply >= (2 * time.Minute), nil
+}
+
+func listNodesInNodepool(n *v1.Node) (*v1.NodeList, error){
+	// Get the NodePool label
+	npName, ok := n.Labels[nodePoolLabel]
+	if !ok {
+		return nil, fmt.Errorf("Node does not have the nodepool label", "nodeName", n.Name, "label", nodePoolLabel)
+	}
+
+	// Create the nodepool label requirement
+	npLabelReq, err := labels.NewRequirement(nodePoolLabel, selection.In, []string{npName})
+	if err != nil {
+		log.Error("Could not create nodepool requirement")
+		return nil, err
+	}
+
+	// Create selector based on the nodepool label requirement
+	opts := &client.ListOptions{
+		LabelSelector: labels.NewSelector().Add(*npLabelReq),
+	}
+
+	// List all nodes in the nodepool
+	nl := &v1.NodeList{}
+	err = r.Client.List(ctx, nl, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return nl, nil
 }
